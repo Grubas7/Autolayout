1fe48c5bbade5c9781c32947eafcb78f598b7804
Adam Grzegorowski <adam.grzegorowski@allegro.pl>
2015-09-14 13:24:59 +0200

    Initial commit.

---
+++ Autolayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Autolayout
+//
+//  Created by Adam Grzegorowski on 14/09/15.
+//  Copyright © 2015 allegro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
