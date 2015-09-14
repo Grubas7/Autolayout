65dc78fd0995dbe77d7021a7f49dc2e4d0b50a49
Adam Grzegorowski <adam.grzegorowski@allegro.pl>
2015-09-14 18:03:24 +0200

    Added dynamic max size of label.

--- Autolayout/SampleView.m
+++ Autolayout/SampleView.m
@@ -5,6 +5,24 @@
 
 #import "SampleView.h"
 
+CG_INLINE CGSize CGSizeIntersect(CGSize size1, CGSize size2) {
+    return CGSizeMake((CGFloat)fmin(size1.width, size2.width), (CGFloat)fmin(size1.height, size2.height));
+}
+
+@interface UIView (sizeThatFitsWithMaxWidth)
+
+- (CGSize)sizeThatFits:(CGSize)size withMaxSize:(CGSize)maxSize;
+
+@end
+
+@implementation UIView (sizeThatFitsWithMaxWidth)
+
+- (CGSize)sizeThatFits:(CGSize)size withMaxSize:(CGSize)maxSize {
+    return CGSizeIntersect([self sizeThatFits:size], maxSize);
+}
+
+@end
+
 @interface SampleView ()
 
 @property (nonatomic, strong) UILabel *rightLabel;
@@ -26,12 +44,12 @@
 
 - (void)setUpSubviews {
     self.rightLabel = [[UILabel alloc] init];
-    self.rightLabel.text = @"Right";
+    self.rightLabel.text = NSLocalizedString(@"Right right, teraz teraz, leci leci", nil);
     self.rightLabel.backgroundColor = [UIColor redColor];
     [self addSubview:self.rightLabel];
 
     self.leftLabel = [[UILabel alloc] init];
-    self.leftLabel.text = @"Left";
+    self.leftLabel.text = NSLocalizedString(@"Left", nil);
     self.leftLabel.backgroundColor = [UIColor greenColor];
     [self addSubview:self.leftLabel];
 }
@@ -45,8 +63,10 @@
 - (void)layoutLabels {
     static CGFloat margin = 10.0f;
     CGFloat viewHeight = CGRectGetHeight(self.frame);
-    CGSize leftLabelSize = [self.leftLabel sizeThatFits:CGSizeZero];
-    CGSize rightLabelSize = [self.rightLabel sizeThatFits:CGSizeZero];
+    CGFloat viewWidth = CGRectGetWidth(self.frame);
+    CGSize leftLabelSize = [self.leftLabel sizeThatFits:CGSizeMake(CGFLOAT_MAX, CGFLOAT_MAX)];
+    CGFloat maxRightLabelWidth = viewWidth - 3.0f * margin - leftLabelSize.width;
+    CGSize rightLabelSize = [self.rightLabel sizeThatFits:CGSizeZero withMaxSize:CGSizeMake(maxRightLabelWidth, CGFLOAT_MAX)];
 
     self.leftLabel.frame = (CGRect){{margin, viewHeight/2.0f}, leftLabelSize};
     self.rightLabel.frame = (CGRect){{CGRectGetMaxX(self.leftLabel.frame) + margin, viewHeight/2.0f}, rightLabelSize};
