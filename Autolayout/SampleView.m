1124a2124af3fe0bae189cb5e375ed6025aea738
Adam Grzegorowski <adam.grzegorowski@allegro.pl>
2015-09-15 15:23:49 +0200

    Replaced static layout by constraint in SampleView.

--- Autolayout/SampleView.m
+++ Autolayout/SampleView.m
@@ -5,24 +5,6 @@
 
 #import "SampleView.h"
 
-CG_INLINE CGSize CGSizeIntersect(CGSize size1, CGSize size2) {
-    return CGSizeMake((CGFloat)fmin(size1.width, size2.width), (CGFloat)fmin(size1.height, size2.height));
-}
-
-@interface UIView (sizeThatFitsWithMaxWidth)
-
-- (CGSize)sizeThatFits:(CGSize)size withMaxSize:(CGSize)maxSize;
-
-@end
-
-@implementation UIView (sizeThatFitsWithMaxWidth)
-
-- (CGSize)sizeThatFits:(CGSize)size withMaxSize:(CGSize)maxSize {
-    return CGSizeIntersect([self sizeThatFits:size], maxSize);
-}
-
-@end
-
 @interface SampleView ()
 
 @property (nonatomic, strong) UILabel *rightLabel;
@@ -37,6 +19,7 @@ CG_INLINE CGSize CGSizeIntersect(CGSize size1, CGSize size2) {
 
     if (self) {
         [self setUpSubviews];
+        [self setUpConstraints];
     }
 
     return self;
@@ -46,30 +29,62 @@ CG_INLINE CGSize CGSizeIntersect(CGSize size1, CGSize size2) {
     self.rightLabel = [[UILabel alloc] init];
     self.rightLabel.text = NSLocalizedString(@"Right right, teraz teraz, leci leci", nil);
     self.rightLabel.backgroundColor = [UIColor redColor];
+    self.rightLabel.translatesAutoresizingMaskIntoConstraints = NO;
     [self addSubview:self.rightLabel];
 
     self.leftLabel = [[UILabel alloc] init];
     self.leftLabel.text = NSLocalizedString(@"Left", nil);
     self.leftLabel.backgroundColor = [UIColor greenColor];
+    self.leftLabel.translatesAutoresizingMaskIntoConstraints = NO;
     [self addSubview:self.leftLabel];
 }
 
-- (void)layoutSubviews {
-    [super layoutSubviews];
+- (void)setUpConstraints {
 
-    [self layoutLabels];
-}
+    CGFloat margin = 10.0f;
+    NSLayoutConstraint *leftLabelCenterY = [NSLayoutConstraint constraintWithItem:self.leftLabel
+                                                                        attribute:NSLayoutAttributeCenterY
+                                                                        relatedBy:NSLayoutRelationEqual
+                                                                           toItem:self
+                                                                        attribute:NSLayoutAttributeCenterY
+                                                                       multiplier:1.0f
+                                                                         constant:0.0f];
 
-- (void)layoutLabels {
-    static CGFloat margin = 10.0f;
-    CGFloat viewHeight = CGRectGetHeight(self.frame);
-    CGFloat viewWidth = CGRectGetWidth(self.frame);
-    CGSize leftLabelSize = [self.leftLabel sizeThatFits:CGSizeMake(CGFLOAT_MAX, CGFLOAT_MAX)];
-    CGFloat maxRightLabelWidth = viewWidth - 3.0f * margin - leftLabelSize.width;
-    CGSize rightLabelSize = [self.rightLabel sizeThatFits:CGSizeZero withMaxSize:CGSizeMake(maxRightLabelWidth, CGFLOAT_MAX)];
+    NSLayoutConstraint *leftLabelMinX = [NSLayoutConstraint constraintWithItem:self.leftLabel
+                                                                     attribute:NSLayoutAttributeLeading
+                                                                     relatedBy:NSLayoutRelationEqual
+                                                                        toItem:self
+                                                                     attribute:NSLayoutAttributeLeading
+                                                                    multiplier:1.0f
+                                                                      constant:margin];
 
-    self.leftLabel.frame = (CGRect){{margin, viewHeight/2.0f}, leftLabelSize};
-    self.rightLabel.frame = (CGRect){{CGRectGetMaxX(self.leftLabel.frame) + margin, viewHeight/2.0f}, rightLabelSize};
+    [self addConstraints:@[ leftLabelCenterY, leftLabelMinX ]];
+
+    NSLayoutConstraint *rightLabelCenterY = [NSLayoutConstraint constraintWithItem:self.rightLabel
+                                                                         attribute:NSLayoutAttributeCenterY
+                                                                         relatedBy:NSLayoutRelationEqual
+                                                                            toItem:self
+                                                                         attribute:NSLayoutAttributeCenterY
+                                                                        multiplier:1.0f
+                                                                          constant:0.0f];
+
+    NSLayoutConstraint *rightLabelMinX = [NSLayoutConstraint constraintWithItem:self.rightLabel
+                                                                      attribute:NSLayoutAttributeLeading
+                                                                      relatedBy:NSLayoutRelationEqual
+                                                                         toItem:self.leftLabel
+                                                                      attribute:NSLayoutAttributeTrailing
+                                                                     multiplier:1.0f
+                                                                       constant:margin];
+
+    NSLayoutConstraint *rightLabelMaxX = [NSLayoutConstraint constraintWithItem:self.rightLabel
+                                                                      attribute:NSLayoutAttributeTrailing
+                                                                      relatedBy:NSLayoutRelationEqual
+                                                                         toItem:self
+                                                                      attribute:NSLayoutAttributeTrailing
+                                                                     multiplier:1.0f
+                                                                       constant:-margin];
+    
+    [self addConstraints:@[ rightLabelCenterY, rightLabelMinX, rightLabelMaxX ]];
 }
 
 @end
