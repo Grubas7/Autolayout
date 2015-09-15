229bf963ea86705b3d1bcf183917e1731954477b
Adam Grzegorowski <adam.grzegorowski@allegro.pl>
2015-09-15 14:41:46 +0200

    Less code repetition for constraints.

--- Autolayout/ViewController.m
+++ Autolayout/ViewController.m
@@ -21,41 +21,27 @@
     [self.view addSubview:sampleView];
 
     CGFloat multiplier = 1.0f;
-    CGFloat constant = 10.0f;
+    CGFloat constant = 0.0f;
+    NSArray *attributes = @[
+        @(NSLayoutAttributeTop),
+        @(NSLayoutAttributeBottom),
+        @(NSLayoutAttributeRight),
+        @(NSLayoutAttributeLeft)
+    ];
 
-    NSLayoutConstraint *topMargin = [NSLayoutConstraint constraintWithItem:sampleView
-                                                                 attribute:NSLayoutAttributeTop
-                                                                 relatedBy:NSLayoutRelationEqual
-                                                                    toItem:self.view
-                                                                 attribute:NSLayoutAttributeTop
-                                                                multiplier:multiplier
-                                                                  constant:constant];
+    for (NSNumber *attributeAsNumber in attributes) {
+        NSLayoutAttribute attribute = attributeAsNumber.integerValue;
 
-    NSLayoutConstraint *bottomMargin = [NSLayoutConstraint constraintWithItem:sampleView
-                                                                    attribute:NSLayoutAttributeBottom
-                                                                    relatedBy:NSLayoutRelationEqual
-                                                                       toItem:self.view
-                                                                    attribute:NSLayoutAttributeBottom
-                                                                   multiplier:multiplier
-                                                                     constant:-constant];
+        NSLayoutConstraint *constraint = [NSLayoutConstraint constraintWithItem:sampleView
+                                                                     attribute:attribute
+                                                                     relatedBy:NSLayoutRelationEqual
+                                                                        toItem:self.view
+                                                                     attribute:attribute
+                                                                    multiplier:multiplier
+                                                                      constant:constant];
 
-    NSLayoutConstraint *rightMargin = [NSLayoutConstraint constraintWithItem:sampleView
-                                                                 attribute:NSLayoutAttributeRight
-                                                                 relatedBy:NSLayoutRelationEqual
-                                                                    toItem:self.view
-                                                                 attribute:NSLayoutAttributeRight
-                                                                multiplier:multiplier
-                                                                  constant:-constant];
-
-    NSLayoutConstraint *leftMargin = [NSLayoutConstraint constraintWithItem:sampleView
-                                                                 attribute:NSLayoutAttributeLeft
-                                                                 relatedBy:NSLayoutRelationEqual
-                                                                    toItem:self.view
-                                                                 attribute:NSLayoutAttributeLeft
-                                                                multiplier:multiplier
-                                                                  constant:constant];
-
-    [self.view addConstraints:@[ topMargin, bottomMargin, rightMargin, leftMargin ]];
+        [self.view addConstraint:constraint];
+    }
 }
 
 @end
